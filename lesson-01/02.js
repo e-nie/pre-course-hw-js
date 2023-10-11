8cacfa3ed0d309d3de87808e0817ef7525f6c60f
Eva Niehaus <eniehaus111@gmail.com>
2023-10-12 00:23:26 +0200

    add 02.js solution

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,7 @@
-// code
+let number = Math.floor(Math.random() * 100);
+
+if (number % 2 === 0) {
+    console.log(number, ':Это четное число');
+} else {
+    console.log(number, ':Это нечетное число');
+}
