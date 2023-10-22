6c52e2dc0fc82f674422898e33063dc05b6234f6
Eva Niehaus <eniehaus111@gmail.com>
2023-10-22 19:55:18 +0200

    add task 5

---
+++ lesson-02/05.js
@@ -0,0 +1,12 @@
+// code
+
+let passport = {
+    name: "Petr",
+    surname: "Petrov",
+};
+
+
+const passportCopy = {...passport}
+passportCopy.name = 'Ivan'
+console.log(passport);
+console.log(passportCopy);
