1136c84f0c56ed346749cd6e734a2ab153ebd5c3
Eva Niehaus <eniehaus111@gmail.com>
2023-10-22 19:25:07 +0200

    add task 4

---
+++ lesson-02/04.js
@@ -0,0 +1,12 @@
+// code
+const giveMeRightCondition = (value1, value2, maximum) => {
+    if (value1 === value2 && value1 <= maximum) {
+        console.log('Success!');
+    } else {
+        console.log('Something wrong!');
+    }
+};
+
+giveMeRightCondition(100, 101, 99);
+
+
