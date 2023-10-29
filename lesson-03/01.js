a2e68cbfd2dfefdcf16c73b3a9741020c69f7868
e-nie <99044956+e-nie@users.noreply.github.com>
2023-10-30 00:56:32 +0100

    add task 1...

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,13 @@
-// code
+
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    const deutscheFlagge = colors.filter(el=> {
+        return el === "черный" || el === "красный" || el === "желтый";
+    })
+    return deutscheFlagge
+}
+
+console.log(createColorString(colors));
+
+
