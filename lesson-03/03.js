9fa3b1b7d88273ac8b6642fbcf319a9cd3b28460
e-nie <99044956+e-nie@users.noreply.github.com>
2023-10-30 01:37:48 +0100

    add task 3

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,9 +1,6 @@
+
+
+
 const fruits = ["яблоко", "банан", "апельсин", "манго", "киви", "ананас", "виноград", "груша", "слива", "персик"];
-
-
-const removeFruits = (arr) => {
-  let res = arr.slice(1, -2)
-   console.log (res)
-};
-
-removeFruits(fruits)
+  let newArr = fruits.slice(1, -2)
+console.log( newArr)
