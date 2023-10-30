0673460b2da759971493e47f8bb560cdacb9dfff
e-nie <99044956+e-nie@users.noreply.github.com>
2023-10-30 18:50:46 +0100

    add task 3

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,5 +1,4 @@
 
-
 const fruits = ["яблоко", "банан", "апельсин", "манго", "киви", "ананас", "виноград", "груша", "слива", "персик"];
   let newFruits = fruits.slice(1, -2)
 console.log( newFruits)
